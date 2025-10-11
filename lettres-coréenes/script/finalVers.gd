4a1f170a3ddf51b653b0da7e9619c71cd54e3693
canigou101 <zombismini.101@gmail.com>
2025-10-11 02:27:20 +0200

    playable game
    le jeu est jouable, sur la partie kr->fr est codé, pas de sound effect, pas de condition de victoire

---
+++ lettres-coréenes/script/finalVers.gd
@@ -0,0 +1,84 @@
+extends Control
+
+var sens=0 #int
+
+var manche =0 #int
+var point=0 #int
+var PoseLettre #int
+
+var listeDeLettres="res://les_lettres/lettres.json" #string
+
+var lettresParse #dictionnaire
+var ListeLettres #liste
+ 
+var lettre_a_trouver # char
+var lettrePropo #char
+
+
+#lecteur de dialogue 
+func chargement_Json(file_path):
+	if FileAccess.file_exists(file_path):
+		var dataFile =FileAccess.open(file_path,FileAccess.READ)
+		
+		var resultat_parse = JSON.parse_string(dataFile.get_as_text())
+		
+		if resultat_parse is Dictionary:
+			return resultat_parse
+		else:
+			print("erreur à la lecture")
+	else: 
+		print("le fichier n'existe pas")
+
+
+func _ready():
+	lettresParse = chargement_Json(listeDeLettres)# chargement du dictionnaire des lettres avec leur corresponcances 
+	ListeLettres=getliste(1,lettresParse)# récupération d'une liste pour pouvoir utiliser la fonction rand pour avoir un index
+	PoseLettre=randomletter(ListeLettres)# récupération d'une position de lettre random
+	affichage()# affichage de la lettre 
+	affichagePoints()# initialisation du compteur
+	
+
+func _on_line_edit_text_submitted(new_text):
+	lettrePropo=new_text
+	isTheSame()
+	generation()
+	%LineEdit.text=""
+	
+
+
+func getliste(sens,dicListe):
+	if sens==1:
+		return dicListe.keys()
+	elif  sens==0:
+		return dicListe.values()
+	return 1
+
+func randomletter(liste):
+	return randi() % len(liste)
+
+func affichage():
+	var string = "la lettre recherché, est : "
+	%Label.text=string + ListeLettres[PoseLettre]
+
+func isTheSame():
+	lettre_a_trouver= lettresParse [ListeLettres[PoseLettre]]
+	if lettrePropo==lettre_a_trouver:
+		manche+=1
+		point+=1
+		print("bonne lettre")
+		print(lettrePropo," ",lettre_a_trouver)
+	elif lettrePropo!=lettre_a_trouver:
+		manche+=1
+		print("mauvaise lettre")
+		print(lettrePropo," ",lettre_a_trouver)
+	affichagePoints()
+
+func affichagePoints():
+	var str="manche: "+str(manche)+"   "+"points: "+str(point) 
+	%manchePoints.text=str
+
+func generation():
+	PoseLettre=randomletter(ListeLettres)# récupération d'une position de lettre random
+	affichage()# affichage de la lettre 
+	affichagePoints()# actualisation du compteur
+	
